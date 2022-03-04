2db468f30d23101c37ad447b7b7f686bb247c61e
nicholas shawn freeman <nick_freeman65@yahoo.com>
2022-03-04 16:58:12 -0600

    commit tree 8098cd0aacfbbdfd1aee60a35eb64747720cbb61

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,24 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def characters
+    self.characters.select('name').map(&:name)
+  end
+
+  def shows
+    self.characters.each do |c|
+        c.shows
+    end
+  end
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.each do |c|
+        "#{c.name} - #{c.show}"
+    end
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,4 @@
 class Show < ActiveRecord::Base
-  
+  has_many :chracters
+  has_many :actors, through: :characters
 end
